081dc4c57a483da7d53b5c36543961c4f69afd18
shohanurrahman1 <shohanur51361@gmail.com>
2022-08-09 14:55:34 +0600

    Module 17 third number problem modified

--- Module-17/third-a.js
+++ Module-17/third-a.js
@@ -1,13 +1,30 @@
-var num1 = 13;
+var num1 = 43;
 var num2 = 79;
 var num3 = 45;
 
-if (num1 >= num2 && num1 >= num3) {
-    console.log('13 is largest');                  
+if (num1 > num2) {
+    if (num1 > num3) {
+        console.log(num1);
+    }
+    else {
+        console.log(num3);
+    }
 }
-else if (num2 >= num1 && num2 >= num3) {
-    console.log('79 is largest');                  
+else {
+    if (num2 > num3) {
+        console.log(num2);
+    }
+    else {
+        console.log(num3);
+    }
 }
-else if (num3 >= num1 && num3 >= num2) {
-    console.log('45 is largest');                  
-}
+
+// if (num1 >= num2 && num1 >= num3) {
+//     console.log('13 is largest');                  
+// }
+// else if (num2 >= num1 && num2 >= num3) {
+//     console.log('79 is largest');                  
+// }
+// else if (num3 >= num1 && num3 >= num2) {
+//     console.log('45 is largest');                  
+// }
