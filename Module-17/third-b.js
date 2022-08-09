42be89aab5473973eb6969e78bcfb4c2e03a92eb
shohanurrahman1 <shohanur51361@gmail.com>
2022-08-09 15:00:38 +0600

    Module 17 third-b some modified

--- Module-17/third-b.js
+++ Module-17/third-b.js
@@ -5,9 +5,16 @@ console.log(side2);
 var side3 = 9;
 console.log(side3);
 
-if (side1 == side2 || side2 == side3 || side3 == side1) {
+if (side1 == side2 || side1 == side3 || side2 == side3) {
     console.log('Triangle is Isosceles');
 }
 else {
     console.log('Triangle is not Isosceles');
 }
+
+// if (side1 == side2 || side2 == side3 || side3 == side1) {
+//     console.log('Triangle is Isosceles');
+// }
+// else {
+//     console.log('Triangle is not Isosceles');
+// }
