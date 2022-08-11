afb1b0de947d21c19f5479ba4210ac476c0a879a
shohanurrahman1 <shohanur51361@gmail.com>
2022-08-11 22:28:02 +0600

    Problem six done

---
+++ Module-18_5/six.js
@@ -0,0 +1,7 @@
+// Run a loop from 30 to 86. this loop will stop if the values get higher than 44
+for (var i = 30; i <= 86; i++) {
+    if (i >= 44) {
+        break;
+    }
+    console.log(i);
+}
