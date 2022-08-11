b4f2f8d46dc6b6ae29ff8e73a31f130d8c2b78e1
shohanurrahman1 <shohanur51361@gmail.com>
2022-08-11 22:23:23 +0600

    Problem five done

---
+++ Module-18_5/five.js
@@ -0,0 +1,9 @@
+// Create an array for all the mobile phones. Display all the elements of the array by using a while loop
+
+var phones = ['Apple', 'Samsung', 'Nokia', 'Lava', 'Blackberry'];
+var i = 0;
+while(i < phones.length) {
+    var phone = phones[i];
+    console.log(phone);
+    i++;
+}
