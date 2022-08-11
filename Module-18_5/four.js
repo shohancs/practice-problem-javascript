7a83ca1d69ccc0cfad6aa7d0ffb828519dde6921
shohanurrahman1 <shohanur51361@gmail.com>
2022-08-11 12:43:22 +0600

    Problem four some modified

--- Module-18_5/four.js
+++ Module-18_5/four.js
@@ -1,5 +1,11 @@
 // Show all odd numbers 581 to 623
 
-for (var i = 581; i <= 623; i += 2) {
-    console.log(i + ' - ' + 'Number is Odd');
+var num = 581;
+while (num <= 623) {
+    console.log(num + ' - ' + 'Number is Odd');
+    num += 2;
 }
+
+// for (var i = 581; i <= 623; i += 2) {
+//     console.log(i + ' - ' + 'Number is Odd');
+// }
