d0ceb5bb505f8449ba2a47dc23cd258a96528817
shohanurrahman1 <shohanur51361@gmail.com>
2022-08-16 21:49:19 +0600

    Problem three some modified

--- Module-19/three.js
+++ Module-19/three.js
@@ -2,7 +2,7 @@
 
 function make_avg() {
     const arrayValue = [10, 20, 30];
-    const average = arrayValue.reduce((a, b) => a + b, 0) / arrayValue.length;
+    const average = arrayValue.reduce((a, b) => a + b, 0) / arrayValue.length; // a:0 b:10 => a:10 b:20 => a:30 b:30 => 60/3=20
     console.log('Average Value is:', average);
     return average;
 }
