5685ba896a9e40a649173faab25e996ca828f683
shohanurrahman1 <shohanur51361@gmail.com>
2022-08-16 08:11:17 +0600

    First Problem Done

---
+++ Module-19/first.js
@@ -0,0 +1,13 @@
+/* 1) Write a function called foo() which prints “foo” and a function called bar() which prints “bar”. Call function bar() in the foo() function after printing. What will be the output? Now call the foo() to see the output. */
+
+// Function Declaration
+function foo() {
+    console.log("foo");
+}
+function bar() {
+    console.log("bar");
+}
+
+// Function Call
+foo();
+bar();
